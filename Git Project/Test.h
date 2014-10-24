dce42ec538b1b753f2caae72d23d5e9acf58a477
Your NameVinh <tqvdesign@gmail.com>
2014-10-24 15:45:58 -0700

    added a test class

---
+++ Git Project/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Project
+//
+//  Created by Vinh Tran on 10/24/14.
+//  Copyright (c) 2014 Libi Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Project/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Project
+//
+//  Created by Vinh Tran on 10/24/14.
+//  Copyright (c) 2014 Libi Studio. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
